4c834a13015a4f429ecdd6de43caf3ced145b418
Ernest Badu <ernestjbadu@outlook.com>
2023-11-26 13:01:02 +0000

    style: format code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,22 @@
 module.exports = {
 	extends: ["@antfu", "turbo", "plugin:svelte/recommended"],
-	parser: '@typescript-eslint/parser',
-  parserOptions: {
-    // ...
-    project: 'tsconfig.base.json',
-    extraFileExtensions: ['.svelte'] // This is a required setting in `@typescript-eslint/parser` v4.24.0.
-  },
-  overrides: [
-    {
-      files: ['*.svelte'],
-      parser: 'svelte-eslint-parser',
-      // Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
-      parserOptions: {
-        parser: '@typescript-eslint/parser'
-      }
-    }
-    // ...
-  ],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		// ...
+		project: "tsconfig.base.json",
+		extraFileExtensions: [".svelte"], // This is a required setting in `@typescript-eslint/parser` v4.24.0.
+	},
+	overrides: [
+		{
+			files: ["*.svelte"],
+			parser: "svelte-eslint-parser",
+			// Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+			parserOptions: {
+				parser: "@typescript-eslint/parser",
+			},
+		},
+		// ...
+	],
 	rules: {
 		"turbo/no-undeclared-env-vars": "off",
 		curly: "off",

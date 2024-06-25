49f4d17fc7a4ef9c6c99e9a32fcbe5dba1021f24
Ernest Badu <ernestjbadu@outlook.com>
2024-06-25 10:17:45 +0100

    fix(example): fix pagination schemas

--- examples/sveltekit/src/lib/hooks/pagination.ts
+++ examples/sveltekit/src/lib/hooks/pagination.ts
@@ -5,17 +5,11 @@ import { z } from "zod";
 // Define the schema for pagination parameters
 export type Pagination = z.infer<typeof Pagination>;
 const Pagination = z.object({
-	page: z.number().default(1),
-	pageSize: z.number().default(10),
+	page: z.coerce.number().default(1),
+	pageSize: z.coerce.number().default(10),
 });
 
 // Create a custom hook for pagination
-export const usePagination = createUseQueryParams(
-	{
-		page: z.number().default(1),
-		pageSize: z.number().default(10),
-	},
-	{
-		adapter: sveltekit(),
-	}
-);
+export const usePagination = createUseQueryParams(Pagination, {
+	adapter: sveltekit(),
+});

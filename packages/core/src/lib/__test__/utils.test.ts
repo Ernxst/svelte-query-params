f36ff09049ac9cc8534b6d159f74890d13b23b0f
Ernest Badu <ernestjbadu@outlook.com>
2024-06-26 10:16:56 +0100

    test: more utils tests

--- packages/core/src/lib/__test__/utils.test.ts
+++ packages/core/src/lib/__test__/utils.test.ts
@@ -1,6 +1,6 @@
-import { parseQueryParams } from "$lib/utils";
+import { debounce, mapValues, parseQueryParams } from "$lib/utils";
 import * as v from "valibot";
-import { describe, expect, test } from "vitest";
+import { assert, beforeEach, describe, expect, test, vi } from "vitest";
 import { z } from "zod";
 
 describe("parseQueryParams", () => {
@@ -76,5 +76,80 @@ describe("parseQueryParams", () => {
 			name: "John Doe",
 			email: "john.doe@mail.co.uk",
 		});
+	});
+});
+
+describe("mapValues", () => {
+	test("should call mapper for every value", () => {
+		const mock = vi.fn().mockReturnValue("foo");
+
+		const result = mapValues(
+			{
+				id: "123",
+				name: "john.doe",
+				email: "john.doe@mail.co.uk",
+			},
+			mock
+		);
+
+		expect(result).toEqual({ id: "foo", name: "foo", email: "foo" });
+		expect(mock).toHaveBeenCalledTimes(3);
+		expect(mock).toHaveBeenCalledWith("123");
+		expect(mock).toHaveBeenCalledWith("john.doe");
+		expect(mock).toHaveBeenCalledWith("john.doe@mail.co.uk");
+	});
+});
+
+describe("debounce", () => {
+	beforeEach(() => {
+		vi.useFakeTimers();
+	});
+
+	test("should debounce a function", () => {
+		const fn = vi.fn().mockImplementation((i) => i);
+		const debounced = debounce(fn, 32);
+
+		const results = [debounced("a"), debounced("b"), debounced("c")];
+		expect(results).toEqual([undefined, undefined, undefined]);
+		expect(fn).not.toHaveBeenCalled();
+
+		setTimeout(() => {
+			expect(fn).toHaveBeenCalledOnce();
+
+			const results = [debounced("d"), debounced("e"), debounced("f")];
+			expect(results).toEqual(["c", "c", "c"]);
+			expect(fn).toHaveBeenCalledOnce();
+		}, 128);
+
+		setTimeout(() => {
+			expect(fn).toHaveBeenCalledTimes(2);
+		}, 256);
+	});
+
+	test("subsequent debounced calls return the last `func` result", () => {
+		const fn = vi.fn().mockImplementation((i) => i);
+		const debounced = debounce(fn, 32);
+		debounced("a");
+		expect(fn).not.toHaveBeenCalled();
+
+		setTimeout(() => {
+			assert.notStrictEqual(debounced("b"), "b");
+			expect(fn).toHaveBeenCalledOnce();
+		}, 64);
+
+		setTimeout(() => {
+			assert.notStrictEqual(debounced("c"), "c");
+			expect(fn).toHaveBeenCalledTimes(2);
+		}, 128);
+	});
+
+	test("should immediately call `func` when `delay` is `0`", () => {
+		const fn = vi.fn().mockImplementation((i) => i);
+		const debounced = debounce(fn, 0);
+
+		debounced();
+		debounced();
+
+		expect(fn).toHaveBeenCalledTimes(2);
 	});
 });

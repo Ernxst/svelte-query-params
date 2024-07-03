b72ac6c55289034f08993aedec9c02323f5fcd92
Ernest Badu <ernestjbadu@outlook.com>
2024-07-03 10:32:32 +0100

    test: add e2e tests for multi-value params

--- packages/core/tests/e2e/specs/e2e.test.ts
+++ packages/core/tests/e2e/specs/e2e.test.ts
@@ -182,3 +182,149 @@ test.describe
 			await expect(subtitle).toHaveText("Count: 10");
 		});
 	});
+
+test.describe("array values", () => {
+	test("should prepopulate from url", async ({ page }) => {
+		await page.goto("/multiselect?categories=electronics&categories=books");
+
+		const [optionsList, resultsList] = await page.getByRole("list").all();
+		const options = await optionsList.getByRole("checkbox").all();
+		const selectedCategories = await resultsList.getByRole("listitem").all();
+
+		expect(options[0]).toBeChecked();
+		expect(options[1]).toBeChecked();
+		expect(options[2]).not.toBeChecked();
+
+		expect(selectedCategories).toHaveLength(2);
+		expect(selectedCategories[0]).toHaveText("electronics");
+		expect(selectedCategories[1]).toHaveText("books");
+	});
+
+	test("should prepopulate from url with a hash", async ({ page }) => {
+		await page.goto(
+			"/multiselect?categories=electronics&categories=books#hash"
+		);
+
+		const [_otionsList, resultsList] = await page.getByRole("list").all();
+		const selectedCategories = await resultsList.getByRole("listitem").all();
+
+		expect(selectedCategories).toHaveLength(2);
+		await expect(selectedCategories[0]).toHaveText("electronics");
+		await expect(selectedCategories[1]).toHaveText("books");
+
+		const url = location(page);
+		expect(url.hash).toEqual("#hash");
+	});
+
+	test("should add to array param", async ({ page }) => {
+		await page.goto("/multiselect");
+
+		const [optionsList, resultsList] = await page.getByRole("list").all();
+		const options = await optionsList.getByRole("checkbox").all();
+		let selectedCategories = await resultsList.getByRole("listitem").all();
+
+		expect(selectedCategories).toHaveLength(0);
+		await expect(options[0]).not.toBeChecked();
+		await expect(options[1]).not.toBeChecked();
+		await expect(options[2]).not.toBeChecked();
+
+		await options[0].click();
+		await options[2].click();
+
+		selectedCategories = await resultsList.getByRole("listitem").all();
+
+		expect(selectedCategories).toHaveLength(2);
+		await expect(selectedCategories[0]).toHaveText("books");
+		await expect(selectedCategories[1]).toHaveText("toys");
+
+		const url = location(page);
+		expect(url.search).toEqual("?categories=books&categories=toys");
+	});
+
+	test("should remove from array param", async ({ page }) => {
+		await page.goto("/multiselect?categories=electronics&categories=books");
+
+		const [optionsList, resultsList] = await page.getByRole("list").all();
+		const options = await optionsList.getByRole("checkbox").all();
+		let selectedCategories = await resultsList.getByRole("listitem").all();
+
+		expect(selectedCategories).toHaveLength(2);
+		await expect(options[0]).toBeChecked();
+		await expect(options[1]).toBeChecked();
+		await expect(options[2]).not.toBeChecked();
+		await expect(selectedCategories[0]).toHaveText("electronics");
+		await expect(selectedCategories[1]).toHaveText("books");
+
+		await options[0].click();
+
+		await expect(options[0]).not.toBeChecked();
+		await expect(options[1]).toBeChecked();
+		await expect(options[2]).not.toBeChecked();
+
+		selectedCategories = await resultsList.getByRole("listitem").all();
+
+		expect(selectedCategories).toHaveLength(1);
+		await expect(selectedCategories[0]).toHaveText("electronics");
+
+		let url = location(page);
+		expect(url.search).toEqual("?categories=electronics");
+
+		await options[1].click();
+
+		await expect(options[0]).not.toBeChecked();
+		await expect(options[1]).not.toBeChecked();
+		await expect(options[2]).not.toBeChecked();
+
+		url = location(page);
+		selectedCategories = await resultsList.getByRole("listitem").all();
+		expect(selectedCategories).toHaveLength(0);
+		expect(url.search).toEqual("");
+
+		await options[0].click();
+
+		await expect(options[0]).toBeChecked();
+		await expect(options[1]).not.toBeChecked();
+		await expect(options[2]).not.toBeChecked();
+
+		selectedCategories = await resultsList.getByRole("listitem").all();
+
+		expect(selectedCategories).toHaveLength(1);
+		await expect(selectedCategories[0]).toHaveText("books");
+
+		url = location(page);
+		expect(url.search).toEqual("?categories=books");
+
+		await options[1].click();
+
+		await expect(options[0]).toBeChecked();
+		await expect(options[1]).toBeChecked();
+		await expect(options[2]).not.toBeChecked();
+
+		selectedCategories = await resultsList.getByRole("listitem").all();
+
+		expect(selectedCategories).toHaveLength(2);
+		await expect(selectedCategories[0]).toHaveText("books");
+		await expect(selectedCategories[1]).toHaveText("electronics");
+
+		url = location(page);
+		expect(url.search).toEqual("?categories=books&categories=electronics");
+
+		await options[2].click();
+
+		await expect(options[0]).toBeChecked();
+		await expect(options[1]).toBeChecked();
+		await expect(options[2]).toBeChecked();
+
+		selectedCategories = await resultsList.getByRole("listitem").all();
+
+		expect(selectedCategories).toHaveLength(3);
+		await expect(selectedCategories[0]).toHaveText("books");
+		await expect(selectedCategories[1]).toHaveText("electronics");
+		await expect(selectedCategories[2]).toHaveText("toys");
+
+		url = location(page);
+		expect(url.search).toEqual(
+			"?categories=books&categories=electronics&categories=toys"
+		);
+	});
+});
